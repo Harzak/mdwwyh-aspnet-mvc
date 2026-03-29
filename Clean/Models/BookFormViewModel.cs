cd9653790dc8d74e242cda786a490013fa52618d
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2026-03-29 20:12:34 +0200

    Add .editorconfig and analyzers

--- Clean/Models/BookFormViewModel.cs
+++ Clean/Models/BookFormViewModel.cs
@@ -40,11 +40,4 @@ namespace Clean.Models
             this.AvailableLibraries = new List<LibrarySelectItemViewModel>();
         }
     }
-
-    public sealed class LibrarySelectItemViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
 }
---
+++ Clean/Models/LibrarySelectItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Clean.Models
+{
+    public sealed class LibrarySelectItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
